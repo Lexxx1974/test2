c251add5822bcae39fc85a2908cbbb94192f45f0
Lex <lexxx1974@gmail.com>
2022-06-27 22:45:46 +1000

    First commit

---
+++ src/Tank.java
@@ -0,0 +1,17 @@
+public class Tank {
+    int position;
+    public Tank(){
+        this(0);
+    }
+    public Tank(int position){
+        this.position = position;
+    }
+
+    void goForward(int pposition){
+        position += pposition;
+    }
+
+    void goBackward(int mposition){
+        position -= mposition;
+    }
+}
